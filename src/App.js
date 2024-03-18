a9df30c8a6f5119e4540e4d9fa077d44b7ef3f18
Ghada3121 <dridighada940@gmail.com>
2024-03-18 01:57:35 +0100

    adding cp-api

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import UserList from "./UserList/UserList";
+import "bootstrap/dist/css/bootstrap.min.css";
+//import ListItems from "./ListItems/ListItems";
+function App() {
+  return (
+    <>
+      <UserList />
+     
+    </>
+  );
+}
+
+export default App;
---
+++ src/UserList/UserList.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ListItems from "../ListItems/ListItems";
+
+import Button from "react-bootstrap/Button";
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+  const getUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data))
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const [user, setUser] = useState({});
+  const handleChange = (e) => {
+    setUser({ ...user, id: Date.now(), name: e.target.value });
+  };
+  const sendUser = () => {
+    axios
+      .get("https://jsonplaceholder.typecode.com/users", user)
+      .then((res) => setUsers(res.data))
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div className="App">
+      <h1> The List Of Users</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="type your name"
+          on
+          Change={handleChange}
+        />
+        <Button className="button" variant="success" onClick={sendUser}>
+          Send
+        </Button>
+
+        <div className="userlist"
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            marginTop: "40px",
+            padding:"20px",
+          }}
+        >
+          {users.map((user, index) => {
+            return <ListItems user={user} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserList;
